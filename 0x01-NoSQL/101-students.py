4a521223c12ec86d4084af5c05d2a0e76d1a84bf
danielsamyy <danielsamy205@gmail.com>
2024-08-26 03:10:23 -0700

    0x01-NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module:Function to prints all students in a collection sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    students = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return students
